22f1e87d31918acd6cc05d97cb5c5ddd15ed98de
GryffindorLi <1697476145@qq.com>
2024-11-01 15:04:10 +0200

    save voiced_flag as well

--- ppg_tts/feature_extract/log_f0_extract.py
+++ ppg_tts/feature_extract/log_f0_extract.py
@@ -14,15 +14,16 @@ if __name__ == "__main__":
     logger.info(f"Extracting log F0 to {args.data_dir}, in total {len(dataset)} utterances.")
 
     with WriteHelper(f"ark,scp:{args.data_dir}/log_f0.ark,{args.data_dir}/log_f0.scp") as writer:
-        for i, utterance in enumerate(dataset):
-            wav = utterance["feature"]
-            foundamental_freq, _, _ = pyin(y=wav.numpy(),
-                                           fmin=125,
-                                           fmax=7600,
-                                           sr=22050,
-                                           fill_na=1e-8,
-                                           hop_length=256,
-                                           frame_length=1024)
-            foundamental_freq = np.log(foundamental_freq.squeeze()[:wav.size(-1)])
-            writer(utterance["key"], foundamental_freq)
-            logger.info(f"{utterance['key']}: wav length {wav.size(-1)}, log_F0 shape {foundamental_freq.shape}")
+        with WriteHelper(f"ark,scp:{args.data_dir}/voiced.ark,{args.data_dir}/voiced.scp") as voiced_writer:
+            for i, utterance in enumerate(dataset):
+                wav = utterance["feature"]
+                foundamental_freq, voiced_flag, _ = pyin(y=wav.numpy(),
+                                                         fmin=125,
+                                                         fmax=7600,
+                                                         sr=22050,
+                                                         hop_length=256,
+                                                         frame_length=1024)
+                foundamental_freq = np.log(foundamental_freq.squeeze())
+                writer(utterance["key"], foundamental_freq)
+                voiced_writer(utterance["key"], voiced_flag.squeeze().astype(np.int32))
+                logger.info(f"{utterance['key']}: wav length {wav.size(-1)}, log_F0 shape {foundamental_freq.shape}")
