af3dbbeb989ae76e0e014e13bcde36561b232775
GryffindorLi <1697476145@qq.com>
2024-11-04 11:16:41 +0200

    add config file

--- ppg_tts/main.py
+++ ppg_tts/main.py
@@ -1,8 +1,10 @@
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch import seed_everything
 from .ConformerTTS_Perso import ConformerTTSModel, PersoDataModule
 
 def cli_main():
     cli = LightningCLI(ConformerTTSModel, PersoDataModule)
 
 if __name__ == "__main__":
+    seed_everything(17, workers=True)
     cli_main()
