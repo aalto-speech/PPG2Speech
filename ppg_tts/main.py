6e912cf4d2d3b3a01afea71373f3febe41c656ea
GryffindorLi <1697476145@qq.com>
2024-11-05 16:08:41 +0200

    reduce lr to fix nan loss

--- ppg_tts/main.py
+++ ppg_tts/main.py
@@ -1,3 +1,4 @@
+import torch
 from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch import seed_everything
 from .ConformerTTS_Perso import ConformerTTSModel, PersoDataModule
@@ -9,4 +10,5 @@ def cli_main():
 
 if __name__ == "__main__":
     seed_everything(17, workers=True)
+    torch.set_float32_matmul_precision('high')
     cli_main()
