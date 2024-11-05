96672d11249f8f895ac9dacccb803e1d348b7ba6
GryffindorLi <1697476145@qq.com>
2024-11-05 16:10:32 +0200

    reduce lr to 1e-3

--- ppg_tts/ConformerTTS_Perso.py
+++ ppg_tts/ConformerTTS_Perso.py
@@ -187,7 +187,7 @@ class ConformerTTSModel(L.LightningModule):
     
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(),
-                                      lr=1e-2)
+                                      lr=1e-3)
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer=optimizer,
             patience=2,
