0f7d54960567e24347db9be5ba3c5dc389ef5f1f
GryffindorLi <1697476145@qq.com>
2024-11-05 17:00:37 +0200

    add model callback

--- ppg_tts/main.py
+++ ppg_tts/main.py
@@ -1,12 +1,22 @@
 import torch
 from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch import seed_everything
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from .ConformerTTS_Perso import ConformerTTSModel, PersoDataModule
 
 def cli_main():
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
+    ck_callback = ModelCheckpoint(dirpath="./exp",
+                                  monitor="val/mel_loss",
+                                  save_top_k=3,
+                                  save_on_train_epoch_end=False,
+                                  auto_insert_metric_name=True,
+                                  mode="min",
+                                  save_last=True)
     cli = LightningCLI(ConformerTTSModel,
                        PersoDataModule,
-                       save_config_kwargs={"overwrite": True})
+                       save_config_kwargs={"overwrite": True},
+                       callbacks=[lr_monitor, ck_callback])
 
 if __name__ == "__main__":
     seed_everything(17, workers=True)
