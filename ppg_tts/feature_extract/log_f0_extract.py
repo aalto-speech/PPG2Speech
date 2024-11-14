b94f082baf714857b66e2f74f4bac782366d3afe
GryffindorLi <1697476145@qq.com>
2024-11-15 00:13:07 +0200

    make log_f0 single thread

--- ppg_tts/feature_extract/log_f0_extract.py
+++ ppg_tts/feature_extract/log_f0_extract.py
@@ -2,7 +2,6 @@ from loguru import logger
 from ..utils import build_parser, extract_f0_from_utterance
 from ..dataset import PersoDatasetBasic
 from kaldiio import WriteHelper
-from multiprocessing import Pool
 
 if __name__ == "__main__":
     parser = build_parser()
@@ -12,11 +11,9 @@ if __name__ == "__main__":
 
     logger.info(f"Extracting log F0 to {args.data_dir}, in total {len(dataset)} utterances.")
 
-    with Pool(args.nj) as pool:
-        results = pool.map(extract_f0_from_utterance, dataset)
-
     with WriteHelper(f"ark,scp:{args.data_dir}/log_f0.ark,{args.data_dir}/log_f0.scp") as writer:
         with WriteHelper(f"ark,scp:{args.data_dir}/voiced.ark,{args.data_dir}/voiced.scp") as voiced_writer:
-            for result in results:
-                writer(result[0], result[1])
-                voiced_writer(result[0], result[2])
+            for d in dataset:
+                key, f0, v_flag = extract_f0_from_utterance(d)
+                writer(key, f0)
+                voiced_writer(key, v_flag)
