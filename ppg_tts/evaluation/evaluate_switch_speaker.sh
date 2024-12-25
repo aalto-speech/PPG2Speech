8209674c326c945ac3bc88f20511d8679c1ce2ac
GryffindorLi <1697476145@qq.com>
2024-12-25 22:01:32 +0200

    update evaluation scripts

--- ppg_tts/evaluation/evaluate_switch_speaker.sh
+++ ppg_tts/evaluation/evaluate_switch_speaker.sh
@@ -18,18 +18,16 @@ python -m ppg_tts.evaluation.switched_spkid_generation --ckpt ${ckpt} --device $
 
 echo "Generating wavs for flipped generated mels"
 
-source activate hifi-gan
-
 python -m vocoder.hifigan.inference_e2e --checkpoint_file vocoder/hifigan/ckpt/g_02500000 --input_mels_dir ${exp_dir}/flip_generate_mel --output_dir ${exp_dir}/flip_generate_wav
 
 cp ${exp_dir}/flip_generate_mel/speaker_mapping ${exp_dir}/flip_generate_wav/speaker_mapping
 
 echo "Calculate speaker embedding distance between original target speaker wavs and generated target speaker wavs"
 
-source activate tts_env
-
 python -m ppg_tts.evaluation.cal_spk_emb_dist --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav --device ${device}
 
+echo "Evaluate WER & CER on the synthesized speech"
 python -m srun python -m ppg_tts.evaluation.evaluate_wer --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav
 
+echo "Evaluate MOS score on the synthesized speech"
 python -m ppg_tts.evaluation.dnsmos_eval --flip_wav_dir ${exp_dir}/flip_generate_wav
