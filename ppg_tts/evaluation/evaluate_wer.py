d385949c1b14709870e30241f02c3f768e0199e0
GryffindorLi <1697476145@qq.com>
2024-12-25 15:49:18 +0200

    update ASR evaluation code

--- ppg_tts/evaluation/evaluate_wer.py
+++ ppg_tts/evaluation/evaluate_wer.py
@@ -1,25 +1,50 @@
 import torch
+import torchaudio
+import os
 from ..utils import build_parser, remove_punc_and_tolower
-from ..dataset import PersoDatasetBasic
 from ..models import PPGFromWav2Vec2Pretrained
 from loguru import logger
-from torchmetrics.functional.text import word_error_rate
+from torchmetrics.functional.text import word_error_rate, char_error_rate
+
+def read_wav_text(wav_dir: str, text_file: str):
+    wavs = os.listdir(wav_dir)
+
+    wavs = [os.path.join(wav_dir, wav) for wav in wavs if "generated_e2e" in wav]
+
+    with open(text_file, "r") as reader:
+        lines = reader.readlines()
+
+    key2text = {}
+
+    for line in lines:
+        key, *text = line.split(' ')
+
+        text = remove_punc_and_tolower(" ".join(text).strip("\n"))
+
+        key2text[key] = text
+
+    for wav in wavs:
+        key = wav.split('/')[-1].replace('_generated_e2e.wav', '')
+        x, sr = torchaudio.load(wav)
+
+        x = torchaudio.functional.resample(x, orig_freq=sr, new_freq=16000)
+
+        yield x, key2text[key], key
 
 if __name__ == "__main__":
     parser = build_parser()
     args = parser.parse_args()
 
-    dataset = PersoDatasetBasic(args.data_dir, 16000)
     ASRModel = PPGFromWav2Vec2Pretrained(args.asr_pretrained)
 
-    logger.info(f"Evaluating WER on {args.data_dir}, in total {len(dataset)} utterances.")
+    logger.info(f"Evaluating WER on {args.flip_wav_dir}, transcripts in {args.data_dir}/text.")
 
     ref = []
     pred = []
 
-    for i, utterance in enumerate(dataset):
-        wav = utterance["feature"]
-        text = remove_punc_and_tolower(utterance['text'])
+    for utterance in read_wav_text(args.flip_wav_dir, f"{args.data_dir}/text"):
+        wav = utterance[0]
+        text = utterance[1]
         ref.append(text)
         ppg = ASRModel.forward(wav)
 
@@ -28,8 +53,10 @@ if __name__ == "__main__":
 
         pred.append(predicted_trans)
 
-        logger.info(f"{utterance['key']}:\nref: {text}\nhyp: {predicted_trans}\n")
+        logger.info(f"{utterance[-1]}:\nref: {text}\nhyp: {predicted_trans}\n")
     
     wer = word_error_rate(pred, ref)
 
-    logger.info(f"Evaluating {len(dataset)} utterances, Word Error Rate {wer.item()}")
+    cer = char_error_rate(pred, ref)
+
+    logger.info(f"Evaluating utterances in {args.flip_wav_dir}, WER {wer.item()}, CER {cer.item()}")
