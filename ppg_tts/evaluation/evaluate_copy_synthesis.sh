e968e253f91e4542940cbfa2c3bfffb8af0949b7
GryffindorLi <1697476145@qq.com>
2025-01-06 10:45:05 +0200

    add stages in evaluation scripts

--- ppg_tts/evaluation/evaluate_copy_synthesis.sh
+++ ppg_tts/evaluation/evaluate_copy_synthesis.sh
@@ -6,32 +6,44 @@ device=$3
 vocoder=$4
 
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 <testset> <ckpt> <device> <vocoder>"
+    echo "Usage: $0 <testset> <ckpt> <device> <vocoder> [<start> <end>]"
     exit 1
 fi
 
+start="${5:-0}"
+end="${6:-5}"
+
 exp_dir=$(realpath $(dirname "$(dirname "$ckpt")"))
 
-echo "Generating mels with flipped speaker identity"
+if [ $start -le 0 ] && [ $end -ge 0 ]; then
+    echo "Generating mels with the same speaker identity"
 
-python -m ppg_tts.evaluation.synthesis --ckpt ${ckpt} --device ${device} --data_dir ${testset}
+    python -m ppg_tts.evaluation.synthesis --ckpt ${ckpt} --device ${device} --data_dir ${testset}
 
-echo "Generating wavs for flipped generated mels"
-
-if [[ $vocoder == "bigvgan" ]]; then
-
-    curr_dir=$(pwd)
-
-    cd vocoder/bigvgan
-    python inference_e2e.py --checkpoint_file bigvgan_generator.pt --input_mels_dir ${exp_dir}/mel --output_dir ${exp_dir[$SLURM_ARRAY_TASK_ID]}/wav_$vocoder
-
-    cd $curr_dir
-else
-    python -m vocoder.hifigan.inference_e2e --checkpoint_file vocoder/hifigan/ckpt/g_02500000 --input_mels_dir ${exp_dir}/mel --output_dir ${exp_dir}/wav_$vocoder
 fi
 
-echo "Evaluate WER & CER on the synthesized speech"
-python -m ppg_tts.evaluation.evaluate_wer --data_dir ${testset} --flip_wav_dir ${exp_dir}/wav_$vocoder
+if [ $start -le 1 ] && [ $end -ge 1 ]; then
+    echo "Generating wavs for generated mels"
 
-echo "Evaluate MOS score on the synthesized speech"
-python -m ppg_tts.evaluation.dnsmos_eval --flip_wav_dir ${exp_dir}/wav_$vocoder
+    if [[ $vocoder == "bigvgan" ]]; then
+
+        curr_dir=$(pwd)
+
+        cd vocoder/bigvgan
+        python inference_e2e.py --checkpoint_file bigvgan_generator.pt --input_mels_dir ${exp_dir}/mel --output_dir ${exp_dir[$SLURM_ARRAY_TASK_ID]}/wav_$vocoder
+
+        cd $curr_dir
+    else
+        python -m vocoder.hifigan.inference_e2e --checkpoint_file vocoder/hifigan/ckpt/g_02500000 --input_mels_dir ${exp_dir}/mel --output_dir ${exp_dir}/wav_$vocoder
+    fi
+fi
+
+if [ $start -le 2 ] && [ $end -ge 2 ]; then
+    echo "Evaluate WER & CER on the synthesized speech"
+    python -m ppg_tts.evaluation.evaluate_wer --data_dir ${testset} --flip_wav_dir ${exp_dir}/wav_$vocoder
+fi
+
+if [ $start -le 3 ] && [ $end -ge 3 ]; then
+    echo "Evaluate MOS score on the synthesized speech"
+    python -m ppg_tts.evaluation.dnsmos_eval --flip_wav_dir ${exp_dir}/wav_$vocoder
+fi
--- ppg_tts/evaluation/evaluate_switch_speaker.sh
+++ ppg_tts/evaluation/evaluate_switch_speaker.sh
@@ -6,38 +6,50 @@ device=$3
 vocoder=$4
 
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 <testset> <ckpt> <device> <vocoder>"
+    echo "Usage: $0 <testset> <ckpt> <device> <vocoder> [<start> <end>]"
     exit 1
 fi
 
+start="${5:-0}"
+end="${6:-5}"
+
 exp_dir=$(realpath $(dirname "$(dirname "$ckpt")"))
 
-echo "Generating mels with flipped speaker identity"
+if [ $start -le 0 ] && [ $end -ge 0 ]; then
+    echo "Generating mels with flipped speaker identity"
 
-python -m ppg_tts.evaluation.synthesis --ckpt ${ckpt} --device ${device} --data_dir ${testset} --switch_speaker
-
-echo "Generating wavs for flipped generated mels"
-
-if [[ $vocoder == "bigvgan" ]]; then
-
-    curr_dir=$(pwd)
-
-    cd vocoder/bigvgan
-    python inference_e2e.py --checkpoint_file bigvgan_generator.pt --input_mels_dir ${exp_dir}/flip_generate_mel --output_dir ${exp_dir[$SLURM_ARRAY_TASK_ID]}/flip_generate_wav_$vocoder
-
-    cd $curr_dir
-else
-    python -m vocoder.hifigan.inference_e2e --checkpoint_file vocoder/hifigan/ckpt/g_02500000 --input_mels_dir ${exp_dir}/flip_generate_mel --output_dir ${exp_dir}/flip_generate_wav_$vocoder
+    python -m ppg_tts.evaluation.synthesis --ckpt ${ckpt} --device ${device} --data_dir ${testset} --switch_speaker
 fi
 
-cp ${exp_dir}/flip_generate_mel/speaker_mapping ${exp_dir}/flip_generate_wav_$vocoder/speaker_mapping
+if [ $start -le 1 ] && [ $end -ge 1 ]; then
+    echo "Generating wavs for flipped generated mels"
 
-echo "Calculate speaker embedding distance between original target speaker wavs and generated target speaker wavs"
+    if [[ $vocoder == "bigvgan" ]]; then
 
-python -m ppg_tts.evaluation.cal_spk_emb_dist --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder --device ${device}
+        curr_dir=$(pwd)
 
-echo "Evaluate WER & CER on the synthesized speech"
-python -m ppg_tts.evaluation.evaluate_wer --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder
+        cd vocoder/bigvgan
+        python inference_e2e.py --checkpoint_file bigvgan_generator.pt --input_mels_dir ${exp_dir}/flip_generate_mel --output_dir ${exp_dir[$SLURM_ARRAY_TASK_ID]}/flip_generate_wav_$vocoder
 
-echo "Evaluate MOS score on the synthesized speech"
-python -m ppg_tts.evaluation.dnsmos_eval --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder
+        cd $curr_dir
+    else
+        python -m vocoder.hifigan.inference_e2e --checkpoint_file vocoder/hifigan/ckpt/g_02500000 --input_mels_dir ${exp_dir}/flip_generate_mel --output_dir ${exp_dir}/flip_generate_wav_$vocoder
+    fi
+    cp ${exp_dir}/flip_generate_mel/speaker_mapping ${exp_dir}/flip_generate_wav_$vocoder/speaker_mapping
+fi
+
+if [ $start -le 2 ] && [ $end -ge 2 ]; then
+    echo "Calculate speaker embedding distance between original target speaker wavs and generated target speaker wavs"
+
+    python -m ppg_tts.evaluation.evaluate_spk_emb_pitch --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder --device ${device}
+fi
+
+if [ $start -le 3 ] && [ $end -ge 3 ]; then
+    echo "Evaluate WER & CER on the synthesized speech"
+    python -m ppg_tts.evaluation.evaluate_wer --data_dir ${testset} --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder
+fi
+
+if [ $start -le 4 ] && [ $end -ge 4 ]; then
+    echo "Evaluate MOS score on the synthesized speech"
+    python -m ppg_tts.evaluation.dnsmos_eval --flip_wav_dir ${exp_dir}/flip_generate_wav_$vocoder
+fi
