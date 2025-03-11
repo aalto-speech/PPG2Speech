aa0f648b2ca5742b35e3087f636d6dfd6a6cdbef
GryffindorLi <1697476145@qq.com>
2025-03-11 18:11:49 +0200

    fall back to kaldi ppg

--- ppg_tts/dataset/generalDataset.py
+++ ppg_tts/dataset/generalDataset.py
@@ -139,8 +139,8 @@ class ExtendDataset(BaseDataset):
         super().__init__(data_dir, target_sr)
         self.no_ctc = no_ctc
 
-        # flag = '_no_ctc' if self.no_ctc else ''
-        flag = '_nn_lsm0.2'
+        flag = '_no_ctc' if self.no_ctc else ''
+        # flag = '_nn_lsm0.2'
         
         self.ppg_path = Path(data_dir, f"ppg{flag}.scp")
         self.spk_emb_path = Path(data_dir, "embedding.scp")
