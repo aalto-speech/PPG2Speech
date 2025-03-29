191fbff643d688dd831e488618ebfa81ad5568ba
GryffindorLi <1697476145@qq.com>
2025-03-29 21:06:38 +0200

    add bash scripts to automate mfa align evaluation

---
+++ ppg_tts/evaluation/evaluate_ppg/mfa_align.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+#SBATCH --time=02:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpu-h200-18g-ia
+#SBATCH --constraint=hopper
+#SBATCH --gpus=1
+#SBATCH --job-name="mfa_align"
+
+corpus=$1
+dictionary=$2
+mfa_model=$3
+output_dir=$4
+
+source activate mfa
+
+mfa align --output_format json -d -j 8 -v --use_mp --clean --final_clean --overwrite \
+    ${corpus} ${dictionary} ${mfa_model} ${output_dir}
